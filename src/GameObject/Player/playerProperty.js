9079011857ce24f7ef7cd940bc8214e935533852
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-09 20:07:37 +0200

    Update playerProperty.js

--- src/GameObject/Player/playerProperty.js
+++ src/GameObject/Player/playerProperty.js
@@ -1,3 +1,16 @@
+import {
+  idle,
+  jump,
+  fall,
+  run,
+  dizzy,
+  sitting,
+  roll,
+  bite,
+  ko,
+  getHit,
+} from "./PlayerStates";
+
 const playerProperty = {
   imageName: "shadow_dog",
   name: "player",
@@ -8,16 +21,16 @@ const playerProperty = {
   speedY: 5,
   zIndex: 1,
   states: {
-    idle: {frameY: 0, frameNrX: 6},
-    jump: {frameY: 1, frameNrX: 6},
-    fall: {frameY: 2, frameNrX: 6},
-    run: {frameY: 3, frameNrX: 8},
-    dizzy: {frameY: 4, frameNrX: 11},
-    sitting: {frameY: 5, frameNrX: 5},
-    roll: {frameY: 6, frameNrX: 7},
-    bite: {frameY: 7, frameNrX: 7},
-    ko: {frameY: 8, frameNrX: 12},
-    getHit: {frameY: 9, frameNrX: 4},
+    [idle]: {frameY: 0, frameNrX: 6},
+    [jump]: {frameY: 1, frameNrX: 6},
+    [fall]: {frameY: 2, frameNrX: 6},
+    [run]: {frameY: 3, frameNrX: 8},
+    [dizzy]: {frameY: 4, frameNrX: 11},
+    [sitting]: {frameY: 5, frameNrX: 5},
+    [roll]: {frameY: 6, frameNrX: 7},
+    [bite]: {frameY: 7, frameNrX: 7},
+    [ko]: {frameY: 8, frameNrX: 12},
+    [getHit]: {frameY: 9, frameNrX: 4},
   },
 };
 
