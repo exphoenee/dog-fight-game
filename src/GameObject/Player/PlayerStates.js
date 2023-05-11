c4b8446b28b1b0074d358e648ee28ca7ce73be48
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-11 19:38:07 +0200

    Update PlayerStates.js

--- src/GameObject/Player/PlayerStates.js
+++ src/GameObject/Player/PlayerStates.js
@@ -1,60 +1,14 @@
-import playerProperty from "./playerProperty";
+import Running from "./PlayerStates/Running";
+import Sitting from "./PlayerStates/Sitting";
+import Jumping from "./PlayerStates/Jumping";
+import Falling from "./PlayerStates/Falling";
 
-export const sitting = "sitting";
-export const walkLeft = "walkLeft";
-export const walkRight = "walkRight";
-export const idle = "idle";
-export const jump = "jump";
-export const fall = "fall";
-export const run = "run";
-export const dizzy = "dizzy";
-export const roll = "roll";
-export const bite = "bite";
-export const ko = "ko";
-export const getHit = "getHit";
+const getPlayerStates = (player) =>
+  [Sitting, Running, Jumping, Falling]
+    .map((State) => new State(player))
+    .reduce((acc, State) => {
+      acc[State.name] = State;
+      return acc;
+    }, {});
 
-export const state = {
-  sitting: 0,
-  STANDING_LEFT: 0,
-  STANDING_RIGHT: 1,
-};
-
-class State {
-  constructor(state, player) {
-    this.name = state;
-    this.player = player;
-  }
-
-  enter() {
-    throw new Error("enter method not implemented");
-  }
-
-  handleInput(input) {
-    throw new Error("handleInput method not implemented");
-  }
-}
-
-class Sitting extends State {
-  constructor(player) {
-    super(sitting, player);
-
-    this.keyboardHandler = player.game.keyboardHandler;
-  }
-
-  enter() {
-    this.player.frameY = playerProperty.states[this.name].frameY;
-  }
-
-  handleInput(keys) {
-    if ([...keys].includes(this.keyboardHandler.keyMap.ArrowLeft)) {
-      this.player.setState(walkLeft);
-    }
-  }
-}
-
-const playerStates = (player) => [new Sitting(player)].reduce((acc, state) => {
-  acc[state.name] = state;
-  return acc;
-}, {});
-
-export default playerStates;
+export default getPlayerStates;
