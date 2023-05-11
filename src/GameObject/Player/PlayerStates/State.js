a77c296c44e1effce41104c494d21176c6fcf6de
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-11 19:38:18 +0200

    Create State.js

---
+++ src/GameObject/Player/PlayerStates/State.js
@@ -0,0 +1,22 @@
+class State {
+  constructor(state, player) {
+    this.name = state;
+    this.player = player;
+    this.keyMap = player.game.keyboardHandler.keyMap;
+  }
+
+  enterActions() {
+    throw new Error("enterActions method not implemented");
+  }
+
+  enter() {
+    this.player.frameY = this.player.states[this.name].frameY;
+    this.enterActions();
+  }
+
+  handleInput() {
+    throw new Error("handleInput method not implemented");
+  }
+}
+
+export default State;
