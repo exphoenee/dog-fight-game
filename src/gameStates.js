d7fa1c8689d86ddce0cf9ba6fbb97fb6df1a8447
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 19:56:50 +0200

    Delete gameStates.js

--- src/gameStates.js
+++
@@ -1,5 +0,0 @@
-export const playing = "playing";
-export const gameOver = "gameOver";
-export const gamePaused = "gamePaused";
-export const loading = "loading";
-export const waitingStart = "waitingStart";
