80e5ca8ddd8676d3fc7cef1d8ecc66f272ac4cd6
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 19:56:32 +0200

    Delete cityLayerProperties.js

--- src/GameObject/CityLayer/cityLayerProperties.js
+++
@@ -1,9 +0,0 @@
-const cityProperties = [
-  {imageName: "city-1", speedModifier: 0.2, name: "layer", zIndex: 100},
-  {imageName: "city-2", speedModifier: 0.4, name: "layer", zIndex: 101},
-  {imageName: "city-3", speedModifier: 0.6, name: "layer", zIndex: 102},
-  {imageName: "city-4", speedModifier: 0.8, name: "layer", zIndex: 103},
-  {imageName: "city-5", speedModifier: 1, name: "layer", zIndex: 104},
-];
-
-export default cityProperties;
