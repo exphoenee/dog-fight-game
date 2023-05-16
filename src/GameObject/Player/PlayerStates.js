c5e61dd633959670eeb898d42ff5ef86ff7f5677
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 20:29:57 +0200

    Update PlayerStates.js

--- src/GameObject/Player/PlayerStates.js
+++ src/GameObject/Player/PlayerStates.js
@@ -2,9 +2,10 @@ import Running from "./PlayerStates/Running";
 import Sitting from "./PlayerStates/Sitting";
 import Jumping from "./PlayerStates/Jumping";
 import Falling from "./PlayerStates/Falling";
+import Rolling from "./PlayerStates/Rolling";
 
 const getPlayerStates = (player) =>
-  [Sitting, Running, Jumping, Falling]
+  [Sitting, Running, Jumping, Falling, Rolling]
     .map((State) => new State(player))
     .reduce((acc, State) => {
       acc[State.name] = State;
