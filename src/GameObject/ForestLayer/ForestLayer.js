36efe9830dd8d5fe6f9ac7e4be44524c69de6516
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 19:56:37 +0200

    Delete ForestLayer.js

--- src/GameObject/ForestLayer/ForestLayer.js
+++
@@ -1,9 +0,0 @@
-import BackgroundLayer from "../BackgroundLayer.js";
-
-class ForestLayer extends BackgroundLayer {
-  constructor(game, {imageName, speedModifier, name}) {
-    super(game, {imageName, speedModifier, name});
-  }
-}
-
-export default ForestLayer;
