3ce0c3017e99e75118f0c187f8e0911bf4d59040
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 16:56:27 +0200

    Update Jumping.js

--- src/GameObject/Player/PlayerStates/Jumping.js
+++ src/GameObject/Player/PlayerStates/Jumping.js
@@ -10,9 +10,8 @@ class Jumping extends State {
 
   enterActions() {
     if (this.player.onGround()) {
-      this.player.jumpSpeed = this.player.jumpHeight;
+      this.player.jumpSpeed = -this.player.jumpHeight;
     }
-    console.log(this.player.jumpHeight, this.player.weight);
   }
 
   handleInput() {
@@ -20,38 +19,6 @@ class Jumping extends State {
       this.player.setState(falling);
     }
   }
-
-  update() {
-    // this.player.positionY -= this.player.jumpSpeed;
-
-    // if (!this.player.onGround) {
-    //   this.player.jumpSpeed -= this.weight;
-    // } else {
-    //   this.player.positionY = this.player.groundLevel;
-    //   this.player.jumpSpeed = 0;
-    // }
-    // console.log(this.player.jumpSpeed, this.player.weight);
-  }
 }
 
 export default Jumping;
-
-function jump() {
-  this.positionY -= this.jumpSpeed;
-
-  if (!this.onGround) {
-    this.jumpSpeed -= this.weight;
-    this.setState("jump");
-  } else {
-    this.positionY = this.groundLevel;
-    this.jumpSpeed = 0;
-  }
-  if (this.game.keyboardHandler.lastKey === "UP_PRESSED") {
-    if (this.onGround) this.jumpSpeed = 30;
-  }
-}
-
-function attack() {
-  this.positionY += this.speedY;
-  this.setState("fall");
-}
