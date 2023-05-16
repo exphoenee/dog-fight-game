bdbf540d84176ab04f1fdc6151c9d51493aadade
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 19:56:30 +0200

    Delete CityLayer.js

--- src/GameObject/CityLayer/CityLayer.js
+++
@@ -1,9 +0,0 @@
-import BackgroundLayer from "../GameObject.js";
-
-class CityLayer extends BackgroundLayer {
-  constructor(game, {imageName, speedModifier, name}) {
-    super(game, {imageName, name});
-  }
-}
-
-export default CityLayer;
