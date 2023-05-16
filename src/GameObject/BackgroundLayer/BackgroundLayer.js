25874c5f1f6a341bb6a270e7b18f7f5eb9c7299f
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 19:56:10 +0200

    Create BackgroundLayer.js

---
+++ src/GameObject/BackgroundLayer/BackgroundLayer.js
@@ -0,0 +1,31 @@
+import GameObject from "../GameObject.js";
+
+class BackgroundLayer extends GameObject {
+  constructor(game, {imageName, speedModifier, name}) {
+    super(game, {imageName, name});
+
+    this.speedModifier = speedModifier;
+    this.speed = this.game.gameSpeed * this.speedModifier;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.positionX2 = this.width;
+
+    this.canvasWidth = this.game.canvasHandler.canvas.width;
+    this.canvasHeight = this.game.canvasHandler.canvas.height;
+  }
+
+  update() {
+    if (this.positionX <= (-this.width * this.canvasHeight) / this.height)
+      this.positionX = Math.floor(this.width + this.positionX2 - this.speed);
+    else this.positionX -= this.speed;
+    if (this.positionX2 <= (-this.width * this.canvasHeight) / this.height)
+      this.positionX2 = Math.floor(this.width + this.positionX - this.speed);
+    else this.positionX2 -= this.speed;
+  }
+
+  draw() {
+    this.game.canvasHandler.drawBackground(this);
+  }
+}
+
+export default BackgroundLayer;
