7951e24fcbb19c505022f3c52696b20dea57ac98
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 20:07:23 +0200

    Update gameStates.js

--- src/Game/gameStates.js
+++ src/Game/gameStates.js
@@ -3,3 +3,5 @@ export const gameOver = "gameOver";
 export const gamePaused = "gamePaused";
 export const loading = "loading";
 export const waitingStart = "waitingStart";
+
+// TODO: refactor it to use a class for every state, like in player states
