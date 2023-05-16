2be747cf55243bb1f74c9341c4efc494c183ebe8
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-16 16:56:31 +0200

    Update State.js

--- src/GameObject/Player/PlayerStates/State.js
+++ src/GameObject/Player/PlayerStates/State.js
@@ -10,15 +10,13 @@ class State {
   }
 
   enter() {
-    this.player.frameY = this.player.states[this.name].frameY;
+    this.player.frameY = this.player.stateAnim[this.name].frameY;
     this.enterActions();
   }
 
   handleInput() {
     throw new Error("handleInput method not implemented");
   }
-
-  update() {}
 }
 
 export default State;
