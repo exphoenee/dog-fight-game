4bdb952ac85fcd819f22b3a747e93ca3bc7c629f
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-19 08:28:40 +0200

    Update Playing.js

--- src/Game/GameStates/Playing.js
+++ src/Game/GameStates/Playing.js
@@ -12,6 +12,19 @@ class Playing extends GameState {
     // Pass
   }
 
+  render() {
+    this.game.gameObjects.sort((a, b) => a.zIndex - b.zIndex);
+    this.game.gameObjects.sort((a, b) => a.positionY - b.positionY);
+    this.game.gameObjects.forEach((obj) => {
+      obj.update();
+      if (this.lastTime >= this.fps) obj.draw();
+    });
+    this.particles.forEach((particle) => {
+      particle.update();
+      if (this.lastTime >= this.fps) particle.draw();
+    });
+  }
+
   handleInput(keys) {
     if (keys.includes(this.keyMap.Esc)) {
       this.game.setState(pause);
