553b19a5351ff8914efdc6243d4d949fbc39cfc8
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-21 17:29:04 +0200

    Create Dizzy.js

---
+++ src/GameObject/Player/PlayerStates/Dizzy.js
@@ -0,0 +1,26 @@
+import PlayerState from "./PlayerState";
+import {sitting} from "./Sitting";
+import {falling} from "./Falling";
+
+export const dizzy = "dizzy";
+
+class Dizzy extends PlayerState {
+  constructor(player) {
+    super(dizzy, player);
+  }
+
+  enterActions() {
+    console.log("dizzy starts");
+    this.game.gameSpeed = 0;
+  }
+
+  handleInput(keys) {
+    console.log("dizzy handleInput");
+    console.log(this.player.frameX, this.player.frameNrX);
+    if (this.player.frameX === 10) {
+      this.player.setState(this.player.onGround() ? sitting : falling);
+    }
+  }
+}
+
+export default Dizzy;
