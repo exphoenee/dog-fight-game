57b31817be95c375150780825970e60373e05c3b
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-21 14:59:20 +0200

    Update gameConfig.js

--- src/Game/gameConfig.js
+++ src/Game/gameConfig.js
@@ -1,12 +1,14 @@
 const gameConfig = {
   width: 800,
   height: 720,
-  gameSpeed: 10,
+  gameSpeed: 0,
+  initGameSpeed: 5,
   gameFrame: 0,
   staggerFrames: 3,
   numberOfEnemies: 4,
   fps: 1000 / 60,
   debugMode: false,
+  particleNumber: 1000,
 };
 
 export default gameConfig;
