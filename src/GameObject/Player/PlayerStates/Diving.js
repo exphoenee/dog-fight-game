1ad98134462e7ab4611b5b2dd5339ef97bc09d22
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-21 15:15:53 +0200

    Update Diving.js

--- src/GameObject/Player/PlayerStates/Diving.js
+++ src/GameObject/Player/PlayerStates/Diving.js
@@ -5,6 +5,7 @@ import {rolling} from "./Rolling";
 export const diving = "diving";
 
 import Fire from "../../Particles/Fire";
+import Splash from "../../Particles/Splash";
 
 class Diving extends PlayerState {
   constructor(player) {
@@ -21,8 +22,15 @@ class Diving extends PlayerState {
       positionX: this.player.positionX,
       positionY: this.player.positionY,
     });
-    if (this.player.onGround()) this.player.setState(running);
-    else if (keys.includes(this.keyMap.Enter)) this.player.setState(rolling);
+    if (this.player.onGround()) {
+      this.player.setState(running);
+      for (let i = 0; i < 30; i++) {
+        new Splash(this.game, {
+          positionX: this.player.positionX,
+          positionY: this.player.positionY,
+        });
+      }
+    } else if (keys.includes(this.keyMap.Enter)) this.player.setState(rolling);
   }
 }
 
