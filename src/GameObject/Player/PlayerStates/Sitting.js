bd5229b93c7443da0380726efee07ef2c875dd3a
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-23 18:20:07 +0200

    Update Sitting.js

--- src/GameObject/Player/PlayerStates/Sitting.js
+++ src/GameObject/Player/PlayerStates/Sitting.js
@@ -16,15 +16,15 @@ class Sitting extends PlayerState {
     this.game.gameSpeed = 0;
   }
 
-  handleInput(keys) {
+  handleInput() {
     if (
-      keys.includes(this.keyMap.ArrowLeft) ||
-      keys.includes(this.keyMap.ArrowRight)
+      this.keys.includes(this.keyMap.ArrowLeft) ||
+      this.keys.includes(this.keyMap.ArrowRight)
     ) {
       this.player.setState(running);
-    } else if (keys.includes(this.keyMap.ArrowUp)) {
+    } else if (this.keys.includes(this.keyMap.ArrowUp)) {
       this.player.setState(jumping);
-    } else if (keys.includes(this.keyMap.Enter)) {
+    } else if (this.keys.includes(this.keyMap.Enter)) {
       this.player.setState(rolling, 2);
     }
     this.player.collisions?.enemy?.length > 0 && this.player.setState(dizzy);
